4debd9d1a5a030199dbcc2319be87d6a19ad25fb
jonny <>
2018-12-21 18:44:49 +0000

    removed bad import and ref to version

--- setup.py
+++ setup.py
@@ -2,14 +2,11 @@
 
 from setuptools import setup
 
-from temperature_logger import __version__
-
-
 REPOSITORY = 'https://github.com/vulcan25/plugable-api'
 
 setup(
     name='plugable-api',
-    version=__version__,
+    #version=__version__,
     description='Plugable API compatibility for flask.',
     author='vulcan25',
     #author_email='',
