5800fef23091570d2086b445b1710a1e091e4880
jonny <>
2018-12-21 18:45:43 +0000

    fixed __version__

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ from setuptools import setup
 
 REPOSITORY = 'https://github.com/vulcan25/plugable-api'
 
+__version__ = 0.1
+
 setup(
     name='plugable-api',
-    #version=__version__,
+    version=__version__,
     description='Plugable API compatibility for flask.',
     author='vulcan25',
     #author_email='',
